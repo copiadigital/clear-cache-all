ffdb3997f637488720c457f29498425125ef9ac5
Jayson Garcia <garciajayson502@gmail.com>
2023-06-15 20:59:54 +0800

    chore: forced to remove page-enhanced

--- src/Caches/ClearAllCaches.php
+++ src/Caches/ClearAllCaches.php
@@ -19,6 +19,24 @@ class ClearAllCaches {
             // clear wordpress cache
             shell_exec('php ' . CLEAR_CACHE_ALL_PLUGIN_DIR . 'wp-cli.phar cache flush --allow-root');
         }
+
+        // delete_w3tc_page_enhanced_cache
+        $this->delete_dir($_SERVER['DOCUMENT_ROOT'] . '/app/cache/page_enhanced');
+    }
+
+    public function clear_all_caches_not_view() {
+        if (function_exists('shell_exec')) {
+            // clear w3 total cache
+            if ( defined( 'W3TC' ) ) {
+                shell_exec('php ' . CLEAR_CACHE_ALL_PLUGIN_DIR . 'wp-cli.phar w3-total-cache flush all --allow-root');
+            }
+            
+            // clear wordpress cache
+            shell_exec('php ' . CLEAR_CACHE_ALL_PLUGIN_DIR . 'wp-cli.phar cache flush --allow-root');
+        }
+
+        // delete_w3tc_page_enhanced_cache
+        $this->delete_dir($_SERVER['DOCUMENT_ROOT'] . '/app/cache/page_enhanced');
     }
 
     private function delete_views_cache() {
@@ -31,6 +49,22 @@ class ClearAllCaches {
                     unlink($file); 
                 }
             }
+        }
+    }
+
+    private function delete_dir($directory) {
+        $subfiles = glob("{$directory}/{,.}*[!.]*", GLOB_MARK|GLOB_BRACE);
+        if($subfiles) {
+            foreach($subfiles as $file)
+            {
+                if(is_dir($file)) { 
+                    $this->delete_dir($file);
+                } else {
+                    unlink($file);
+                }
+            }
+
+            return rmdir($directory);
         }
     }
 
--- src/Providers/AutomateClearCacheServiceProvider.php
+++ src/Providers/AutomateClearCacheServiceProvider.php
@@ -18,6 +18,6 @@ class AutomateClearCacheServiceProvider implements Provider
 
     public function clear_cache_after_save_post() {
         $clear_all_caches = new ClearAllCaches();
-        $clear_all_caches->clear_all_caches();
+        $clear_all_caches->clear_all_caches_not_view();
     }
 }
